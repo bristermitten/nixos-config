5194f76e8a76942d0ea5cc0212893613cfd3f200
Alexander Wood <alexljwood24@hotmail.co.uk>
2024-12-11 17:25:41 +0000

    firewall

---
+++ modules/nixos/firewall.nix
@@ -0,0 +1,7 @@
+{
+networking.nftables.enable = true;
+
+
+   networking.firewall.allowedTCPPorts = [ 80 443 3000 996 7946 4789 2377];
+   networking.firewall.allowedUDPPorts = [ 7946 4789 2377 ];
+}
