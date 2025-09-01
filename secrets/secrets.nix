38a97de37af25b9c4b80cf539f42ab95eef7714c
Alexander Wood <alexljwood24@hotmail.co.uk>
2025-09-01 10:38:56 +0000

    update secrets ssh keys to include new server

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,4 +1,4 @@
-let keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNPRzSLlLpDQwA9QpurZnREyON6z9wHiNJ9tZexFmgh alex@Alexs-MacBook-Pro.local" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbR6nZNH5j1AKVURwjyF2Pyhu/aAgttiu8eIOhiG2mn alex@vmi2319146" ]; in
+let keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPNPRzSLlLpDQwA9QpurZnREyON6z9wHiNJ9tZexFmgh alex@Alexs-MacBook-Pro.local" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKbR6nZNH5j1AKVURwjyF2Pyhu/aAgttiu8eIOhiG2mn alex@vmi2319146" "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDkZPyFLqFM05Q8fbAsnxGn+ViWNuHrlpgQ+y++85QMe alex@vmi2780625" ]; in
 {
   "secrets.age".publicKeys = keys;
   "coolify.age".publicKeys = keys;
